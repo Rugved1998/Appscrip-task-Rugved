8f7847aec14f55017f0988c2ffbce80911ebb398
Aradhika Ramesh <rugved.bmw@gmail.com>
2025-01-08 08:53:37 +0530

    Basic Header and Placeholders setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,71 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+// import style from ""
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <>
+    <div className={styles.TopBorder}>
+      Top Border
     </div>
+    <div className={styles.Header}>
+      
+      <div className={styles.LogoBar}>
+      <div>
+        <Image
+        src="/Logo.png"
+        alt="Logo"
+        width={36}
+        height={36}
+        />
+        </div>
+        
+        <div>
+          LOGO
+        </div>
+        <div>
+          Icons
+        </div>
+      </div>
+      
+      <div className={styles.HeaderContent}>
+        <div>
+          SHOP
+        </div>
+        <div>
+          SKILLS
+        </div>
+        <div>
+          STORIES
+        </div>
+        <div>
+          ABOUT
+        </div>
+        <div>
+          CONTACT US
+        </div>
+      </div>
+    </div>
+    <hr/>
+    <div>
+      Hero Section
+    </div>
+    <div>
+      Content
+        <div>
+           Grid Titlebar
+        </div>
+      <div>
+        Grid
+          <div>
+            Filters
+          </div>
+          <div>
+            Products
+          </div>
+      </div>
+    </div>
+    </>
+   
   );
 }
